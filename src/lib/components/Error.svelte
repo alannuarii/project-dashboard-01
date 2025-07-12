38e988eff322ca3f28fec1ccaa0c02a093090250
alannuarii <alannuariii@gmail.com>
2025-07-12 14:19:36 +0800

    Update

--- src/lib/components/Error.svelte
+++ src/lib/components/Error.svelte
@@ -1,12 +1,12 @@
 <script>
-	import error from '../img/error.gif';
+	import error from '../img/error.png';
 	export let message = '';
 </script>
 
 {#if message}
 	<div class="container-fluid">
-		<div class="d-flex justify-content-center">
-			<img src={error} class="img-fluid rounded-5" alt="" />
+		<div class="d-flex justify-content-center mb-3">
+			<img src={error} class="img-fluid" alt="" />
 		</div>
 		<div class="d-flex justify-content-center">
 			<button
